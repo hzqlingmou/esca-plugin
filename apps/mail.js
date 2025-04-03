92689222ce5df3f4d960e375d581efde770c4dda
hzqlingmou <2833598659@qq.com>
2025-04-04 07:30:16 +0800

    支持多行

--- apps/mail.js
+++ apps/mail.js
@@ -5,6 +5,10 @@ import { SettingsFunc } from '../lib/config.js';
 const settings = new SettingsFunc();
 const sender = new MailSender();
 
+let receiver = '';
+let subject = '';
+let content = '';
+
 export class esca_mail extends plugin {
     constructor() {
         super({
@@ -19,10 +23,18 @@ export class esca_mail extends plugin {
                 },
                 {
                     reg: /^(#|e)发送邮件\s*([^\s:@]+@[^\s:@]+\.[^\s:@]+):([^:]+):([\s\S]+)/,
-                    fnc: 'sendCustomMail'
+                    fnc: 'sendCustomMailOneLine'
                 },
+                {
+                    reg: '^(#|e)发送邮件$',
+                    fnc: 'sendCustomMail'
+                }
             ]
         });
+
+        this.receiver = 0;
+        this.subject = 0;
+        this.content = 0
     }
 
     async checkMail(e) {
@@ -38,16 +50,72 @@ export class esca_mail extends plugin {
         }
     }
 
-    async sendCustomMail(e) {
+    async sendCustomMailOneLine(e) {
         const isMgr = await settings.checkAuth(e);
         if (!(isMgr)) return
-        const [fullMatch, prefix, receiver, subject, content] = e.msg.match(/^(#|e)发送邮件\s*([^\s:@]+@[^\s:@]+\.[^\s:@]+):([^:]+):([\s\S]+)/);
-        const MailSendStatus = await sender.SendMail(e, 'custom', content, subject, 'custom.html', receiver);
+        const [fullMatch, prefix, onelineReceiver, onelineSubject, onelineContent] = e.msg.match(/^(#|e)发送邮件\s*([^\s:@]+@[^\s:@]+\.[^\s:@]+):([^:]+):([\s\S]+)/);
+        const MailSendStatus = await sender.SendMail(e, 'custom', onelineContent, onelineSubject, 'custom.html', onelineReceiver);
         if (MailSendStatus) {
             e.reply('邮件发送成功');
             return true;
         } else {
             e.reply('邮件发送失败，请查看日志');
+            return true;
+        }
+    }
+
+    async sendCustomMail(e) {
+        try {
+            receiver = '';
+            subject = '';
+            content = '';
+            this.setContext('getReceiver')
+            return e.reply('请输入收件人邮箱地址');
+        } catch (error) {
+            logger.error(error);
+            return true;
+        }
+    }
+
+    async getReceiver(e) {
+        try {
+            this.finish('getReceiver')
+            receiver = this.e.msg;
+            this.setContext('getSubject')
+            return e.reply('请输入邮件主题');
+        } catch (error) {
+            logger.error(error);
+            return true;
+        }
+    }
+
+    async getSubject(e) {
+        try {
+            this.finish('getSubject')
+            subject = this.e.msg;
+            this.setContext('getContent')
+            return e.reply('请输入邮件内容');
+        } catch (error) {
+            logger.error(error);
+            return true;
+        }
+    }
+
+    async getContent(e) {
+        try {
+            this.finish('getContent')
+            content = this.e.msg;
+            e.reply('正在发送邮件，请稍后...')
+            const MailSendStatus = await sender.SendMail(e, 'custom', content, subject, 'custom.html', receiver);
+            if (MailSendStatus) {
+                e.reply('邮件发送成功');
+                return true;
+            } else {
+                e.reply('邮件发送失败，请查看日志');
+                return true;
+            }
+        } catch (error) {
+            logger.error(error);
             return true;
         }
     }
